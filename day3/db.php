55480db10c8079ea0863572425d2f5f318f2615f
Phan Tấn Hòa <12c7phantanhoa@gmail.com>
2025-07-19 21:44:26 +0700

    Add files via upload

---
+++ day3/db.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'ex03';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die('Kết nối thất bại: ' . mysqli_connect_error());
+}
+?> 
